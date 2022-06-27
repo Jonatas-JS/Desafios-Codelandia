8300d7e542b3eef9652e56bcaec89098878d6d7c
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-06-27 16:18:18 -0300

    desafio 01 concluído
    Feito o que restava do JS e implementado o like com o checkbox como avia mencionado antes.

--- desafio-01-blog/src/script.js
+++ desafio-01-blog/src/script.js
@@ -31,10 +31,22 @@ for (let i = 0; i < postsList.length; i++) {
   <div class="post">
     <div class="post-header">
       <p class="post-date">${postsList[i].date}</p>
-      <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M133.7,211.9l81-81c19.9-20,22.8-52.7,4-73.6a52,52,0,0,0-75.5-2.1L128,70.5,114.9,57.3c-20-19.9-52.7-22.8-73.6-4a52,52,0,0,0-2.1,75.5l83.1,83.1A8.1,8.1,0,0,0,133.7,211.9Z" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="17" class="post-like"></path></svg></span>
-    </div>
+        <label for="checkbox" class="likeCheckbox">
+          <input type="checkbox" class="checkbox">
+          <span class="like"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M133.7,211.9l81-81c19.9-20,22.8-52.7,4-73.6a52,52,0,0,0-75.5-2.1L128,70.5,114.9,57.3c-20-19.9-52.7-22.8-73.6-4a52,52,0,0,0-2.1,75.5l83.1,83.1A8.1,8.1,0,0,0,133.7,211.9Z" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="17" class="post-like"></path></svg></span>
+        </label>
+      </div>
     <h3 class="post-title">${postsList[i].title}</h3>
     <p class="post-description">${postsList[i].description}</p>
   </div>
   `
 }
+
+// function like(){
+//   let likeStatus = document.querySelector('.like')
+//   if(likeStatus = document.querySelector('.like .active')){
+//     likeStatus.classList.remove('active')
+//   } else {
+//     likeStatus.classList.add('active')
+//   }
+// }
