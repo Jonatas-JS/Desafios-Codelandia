d409abbe65ee5e9b4bf7d7f026c89f6aea0ad35b
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-06-28 08:52:28 -0300

    date adjustment

--- desafio-01-blog/src/script.js
+++ desafio-01-blog/src/script.js
@@ -3,25 +3,25 @@ const postsList = [
     id: 1,
     title: 'Agora é oficial: o Windows 11 está vindo',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.',
-    date: '12 de jul, 2021',
+    date: '12 de jun, 2022',
   },
   {
     id: 2,
     title: 'Tim Berners-Lee vai leiloar código-fonte da web',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.',
-    date: '09 de jul, 2021',
+    date: '09 de jun, 2022',
   },
   {
     id: 3,
     title: 'Tem Firefox novo no pedaço e você vai querer migrar',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.',
-    date: '09 de jul, 2021',
+    date: '09 de jun, 2022',
   },
   {
     id: 4,
     title: 'John McAfee, criador do antivírus McAfee, morre',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.',
-    date: '07 de jul, 2021',
+    date: '07 de jun, 2022',
   },
 ]
 
