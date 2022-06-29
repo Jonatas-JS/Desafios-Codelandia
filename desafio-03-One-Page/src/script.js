1fed62be01d4e1c8a3d94fa951bedf29bd897852
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-06-29 17:13:35 -0300

    finalizado script
    Finalizado o script para gerar os cards automaticamente e gerar as cores dos cardes aleatoriamente.
    Resta ainda trabalhar nas soluções de resposividade.

--- desafio-03-One-Page/src/script.js
+++ desafio-03-One-Page/src/script.js
@@ -1,12 +1,24 @@
-function generateColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function cardsGeneration() {
+  const cards = document.querySelector('.cardsList')
+  for ( let i = 0; i < 6; i++) {
+    cards.innerHTML += `
+    <div class="card">
+    <div class="cardColor" style="background: ${generateColor()}"></div>
+    <div class="cardDescription">
+      <h2>Título do card</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in neque et nisl.</p>
+    </div>
+  </div>
+    `
   }
-  
-  return color;
-  
-}
-console.log(generateColor())
+
+  function generateColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+} cardsGeneration()
