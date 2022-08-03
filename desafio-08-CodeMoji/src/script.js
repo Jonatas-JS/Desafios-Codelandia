0f0089325412bea9b3bc3e0eb94c52860f4b2ead
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-08-03 11:29:39 -0300

    iniciado desafio 08

---
+++ desafio-08-CodeMoji/src/script.js
@@ -0,0 +1,40 @@
+let images = [
+  {
+    id: 1,
+    image: './src/assets/memoji-01.svg',
+    bgImage: './src/assets/memoji-01-form.svg',
+  },
+  {
+    id: 2,
+    image: './src/assets/memoji-02.svg',
+    bgImage: './src/assets/memoji-02-form.svg',
+  },
+  {
+    id: 3,
+    image: './src/assets/memoji-03.svg',
+    bgImage: './src/assets/memoji-03-form.svg',
+  },
+  {
+    id: 4,
+    image: './src/assets/memoji-04.svg',
+    bgImage: './src/assets/memoji-04-form.svg',
+  },
+  {
+    id: 5,
+    image: './src/assets/memoji-05.svg',
+    bgImage: './src/assets/memoji-05-form.svg',
+  },
+]
+
+function renderImages() {
+  let bloco = document.querySelector('.image-bg')
+  for(i = 0; i < images.length; i++) {
+    bloco.innerHTML += `
+      <div class="image">
+        <img src="${images[i].image}"/>
+        <span><img src="${images[i].bgImage}"/></span>
+      </div>
+    `
+  }
+}
+renderImages()
